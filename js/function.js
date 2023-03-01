ddce2c9e9e2ade6ec29a8f5211e85e3822cb81e3
Radik <radik.usatu@gmail.com>
2023-03-01 23:41:16 +0500

    2 попытка

--- js/function.js
+++ js/function.js
@@ -1,36 +1,31 @@
-const isLengthString=(string, length) => {
-  if (string.length<=length) {
-    return true;
-  }
-  return false;
-}
-const isPalindrom=(string)=> {
-  const LowerString=string.toLowerCase();
-  let reverce='';
-  for (let i=LowerString.length-1;i>=0;i--) {
-   reverce+=LowerString.at(i);
+const isLengthString = (string, length) => (string.length <= length);
+function isPalindrom(string) {
+  const lowerString = string.toLowerCase();
+  let reverse = '';
+  for (let i = lowerString.length - 1; i >= 0; i--) {
+    reverse += lowerString.at(i);
 
 
   }
-  return LowerString===reverce;
+  return lowerString === reverse;
 }
 
-const exNumber= (string)=> {
-  let result='';
-  for (let i=0;i<string.length;i++) {
+const extractNumber = (string)=> {
+  let result = '';
+  for (let i = 0;i < string.length;i++) {
     if (!Number.isNaN(parseInt(string.at(i),10))) {
-    result+=string.at(i);
+      result += string.at(i);
 
+    }
   }
-}
-return parseInt(result,10)
-}
-const myPadStart = (string, minLength, pad) => {
-  const actualPad=minLength-string.length;
-if (actualPad<=0) {
-  return string;
-}
+  return parseInt(result,10);
+};
+const lineFilling = (string, minLength, pad) => {
+  const actualPad = minLength - string.length;
+  if (actualPad <= 0) {
+    return string;
+  }
 
-return pad.slice(0, actualPad% pad.length) + pad.repeat(actualPad/pad.length)+string;
+  return pad.slice(0, actualPad % pad.length) + pad.repeat(actualPad / pad.length) + string;
 
-}
+};
